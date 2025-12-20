dd20a389fbcbb605e4ee41ce96928b4af2073a3b
Hajid Raihan <hajidraihan@gmail.com>
2025-12-21 00:50:36 +0700

    alskdj

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Product</li>
+        <li>Cart</li>
+      </ul>
+
+      <button>Logout</button>
+    </div>
+  );
+};
